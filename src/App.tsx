46edeb543afef1b3bb12ef1cc821ea3d16e2cadf
arolson101 <andrew@olsononline.org>
2025-01-25 09:48:09 -0600

    vite/hono integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello, Hono with React!</h1>
+      <h2>Example of useState()</h2>
+      <Counter />
+      <h2>Example of API fetch()</h2>
+      <ClockButton />
     </>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      You clicked me {count} times
+    </button>
+  );
+}
+
+const ClockButton = () => {
+  const [response, setResponse] = useState<string | null>(null);
+
+  const handleClick = async () => {
+    const response = await fetch("/api/clock");
+    const data = await response.json();
+    const headers = Array.from(response.headers.entries()).reduce(
+      (acc, [key, value]) => ({ ...acc, [key]: value }),
+      {}
+    );
+    const fullResponse = {
+      url: response.url,
+      status: response.status,
+      headers,
+      body: data,
+    };
+    setResponse(JSON.stringify(fullResponse, null, 2));
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Get Server Time</button>
+      {response && <pre>{response}</pre>}
+    </div>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
+import build from "@hono/vite-build/node";
+import devServer from "@hono/vite-dev-server";
+import adapter from "@hono/vite-dev-server/node";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { config } from "dotenv";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      build: {
+        rollupOptions: {
+          input: "./src/client.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      ssr: {
+        external: ["react", "react-dom"],
+      },
+      plugins: [
+        build({
+          outputDir: "server-build",
+        }),
+        devServer({
+          env,
+          adapter,
+          entry: "src/index.tsx",
+        }),
+      ],
+    };
+  }
 });
+
+function env() {
+  const result = config({
+    override: true,
+    // debug: true,
+    encoding: "utf8",
+  });
+  if (result.error) {
+    throw result.error;
+  }
+  return result.parsed!;
+}
