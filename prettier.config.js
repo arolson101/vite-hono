ccd1b4dee9faf141c19389a2e1b84345521421bd
arolson101 <andrew@olsononline.org>
2025-03-02 14:04:31 -0600

    add react-scan

--- prettier.config.js
+++ prettier.config.js
@@ -7,6 +7,7 @@ const config = {
   ],
   importOrder: [
     'server-only', //
+    '^react-scan$',
     '^@core/(.*)$',
     '<THIRD_PARTY_MODULES>',
     '^@server/(.*)$',
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,14 @@
+import { scan } from 'react-scan'
 import { StartClient } from '@tanstack/start'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createRouter } from './router'
+
+if (import.meta.env.DEV) {
+  scan({
+    enabled: import.meta.env.DEV,
+  })
+}
 
 const router = createRouter()
 
